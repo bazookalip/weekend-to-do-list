1073e9c1a74bb8baa921eb5bd7d6f923255b35cb
Marcus Anderberg <marcusanderberg@gmail.com>
2019-04-03 18:40:44 -0500

    changed name of port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 const router = require('./routes/tasks.router')
@@ -10,5 +10,5 @@ const router = require('./routes/tasks.router')
 app.use('/tasks', router)
 
 app.listen(PORT, () => {
-    console.log(`Running on port: ${port}`);
+    console.log(`Running on port: ${PORT}`);
 });
